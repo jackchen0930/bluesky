870965a10ca2a7b58abe676840a2052d99a7ed87
chentao <chentao@smzdm.com>
2017-09-23 16:25:36 +0800

    V1.0.0 我的library  提交 library库文件

---
+++ myopenpro/bluesky_v/src/main/java/com/hwj/android/client/TestAA.java
@@ -0,0 +1,13 @@
+package com.hwj.android.client;
+
+
+/**
+ * Created by Chentao on 2017/9/23.
+ */
+
+public class TestAA {
+
+    public double addition(double additon1,double addition2){
+        return additon1+addition2;
+    }
+}
